881557011b779e08d0c7ccba517ead8e0d496a9a
Karen Bonfils <karen.bonfils@heig-vd.ch>
2022-03-25 10:12:13 +0100

    exercice04 avancement

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,8 +1,24 @@
 import * as d3 from 'd3'
+import { csv, json } from 'd3-fetch'
 
 // Pour importer les données
 // import file from '../data/data.csv'
+console.log("bonjour");
 
+//récupération des données
+
+d3.json('../data/life_expectancy_years.csv')
+		.then( function(data) {
+            console.log(data)
+				const notesParEleve = data.reduce((r, d) => {
+  				const notes = r[d.nom] || []
+  				return { ...r, [d.nom]: [...notes, d.note] }
+			}, {})
+
+			 	})
+		.catch(function(error){
+				console.log(error);
+				})
 
 
 
