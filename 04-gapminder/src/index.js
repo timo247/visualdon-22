216ad8fd7e085c1d58fc270c7f6ed76d65a6292b
Timothée Dione <timothee.dione@gmail.com>
2022-03-25 11:39:20 +0100

    graphiques affichés correctement

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3'
+import { svg } from 'd3';
 import { csv, json } from 'd3-fetch'
 // Pour importer les données
 // import file from '../data/data.csv'
@@ -13,12 +14,10 @@ d3.json('../data/life_expectancy_years.csv')
   				const notes = r[d.nom] || []
   				return { ...r, [d.nom]: [...notes, d.note] }
 			}, {})
-
 			 	})
 		.catch(function(error){
 				console.log(error);
 				})
-
 
 Promise.all([
     csv('income_per_person_gdppercapita_ppp_inflation_adjusted.csv'),
@@ -26,18 +25,19 @@ Promise.all([
 ])
     .then(([income, lifeExpectency]) => {
     console.log(income, lifeExpectency)
- 
-
     //dessin
     const margin = { top: 10, right: 40, bottom: 10, left: 40 },
     width = 450 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
+    let translateHeight = height + 10;
+    let translateWidth = width + 10;
     let svg = d3.select(".graphique");
 
 
 svg.attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
+    .attr("class", "firstGroup")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 const x = d3.scaleLinear()
@@ -48,7 +48,7 @@ const x = d3.scaleLinear()
     //axe x
 svg.append('g')
     .attr('class', "xAxis")
-    .attr("transform", "translate(100," + height + ")")
+    .attr("transform", "translate(100," + translateHeight + ")")
     .call(d3.axisBottom(x));
 
 const y = d3.scaleLinear()
@@ -59,7 +59,7 @@ const y = d3.scaleLinear()
 svg.append('g')
     .call(d3.axisLeft(y))
     .attr('class', "yAxis")
-    .attr("transform", "translate(100, 0)")
+    .attr("transform", "translate(100, 10)")
 
 
 svg.append("circle")
@@ -68,8 +68,16 @@ svg.append("circle")
     .attr("cx", x(50)).attr("cy", y(60)).attr("r", 40).style("fill", "red");
 svg.append("circle")
     .attr("cx", x(100)).attr("cy", y(60)).attr("r", 40).style("fill", "green");
-    });
 
+
+    //Réagrandissement du svg pour que les axes s'affichent complètement
+    let svgToGrow = document.querySelector(".graphique");
+    console.log(svgToGrow)
+    let newHeight = height + 20
+    svgToGrow.setAttribute("height", "420")
+    console.log(svgToGrow)
+});
+  
 /*
 d3.csv('income_per_person_gdppercapita_ppp_inflation_adjusted.csv')
            .then( function(data) {
