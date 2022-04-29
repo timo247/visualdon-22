e0222cf16474d8086648584f46aaed527f6e5eed
Timothée Dione <timothee.dione@gmail.com>
2022-04-29 11:56:39 +0200

    web scrapping e commerce fonctionne

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -2,6 +2,10 @@ import jsdom from "jsdom";
 import fetch from "isomorphic-fetch"
 import puppeteer from "puppeteer"
 
+
+const items = {
+    list:[]
+}
 /*Cours_
 // Lancement browser
 const browser = await puppeteer.launch();
@@ -34,49 +38,91 @@ await browser.close();
 */
 
 //capture d'écran des cantons
-async function screenShotCantons () {
+async function screenShotCantons() {
     // Lancement browser
-      const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch();
     //Ouvrir une nouvelle page
-      const page = await browser.newPage();
-      //console.log(page)
+    const page = await browser.newPage();
+    //console.log(page)
     await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
-     
+
     //Sélecteurs
     const tableCantons = await page.$('table.wikitable'); // document.querySelector
-    await tableCantons.screenshot({ path: '././img/photoDesCantons.png' }); 
+    await tableCantons.screenshot({ path: '././img/photoDesCantons.png' });
     // Fermer les browser
     await browser.close();
-    };
-
-
-    async function showCantonsOnConsole(){
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
-        const tableCantons = await page.$('table.wikitable'); // document.querySelector
+};
 
 
 
-        const result = await page.$$eval('table.wikitable tr', rows => {
-            return Array.from(rows, row => {
-              const columns = row.querySelectorAll('td');
-              return Array.from(columns, column => column.innerText);
-            });
-          });
-        console.log(result)
+//Affichage de la liste des cantons dans la console
+async function showCantonsOnConsole() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
 
-        const cantonsTable = []
-
-        result.forEach(canton => {
-            let newCanton = [];
-            newCanton["canton"] = canton[0];
-            newCanton["population"] = canton[3];
-            cantonsTable.push(newCanton);
+    const result = await page.$$eval('table.wikitable tr', rows => {
+        return Array.from(rows, row => {
+            const columns = row.querySelectorAll('td');
+            return Array.from(columns, column => column.innerText);
         });
+    });
+    console.log(result)
 
-        console.table(cantonsTable)
-    }
+    const cantonsTable = []
 
-   // screenShotCantons();
-   //showCantonsOnConsole();
+    result.forEach(canton => {
+        let newCanton = [];
+        newCanton["canton"] = canton[0];
+        newCanton["population"] = canton[3];
+        cantonsTable.push(newCanton);
+    });
+
+    console.table(cantonsTable)
+}
+
+
+//Webscrap E-commerce
+async function scrapECommerce() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+
+
+    //     const result = await page.$$eval('div.thumbnail', rows => {
+    //         return Array.from(rows, row => {
+    //           const columns = row.querySelectorAll('td');
+    //           return Array.from(columns, column => column.innerText);
+    //         });
+    //       });
+    //     console.log(result)
+
+
+    const result = await page.$$('div.thumbnail');
+    result.forEach(async item => {
+        let priceScrap = await item.$('h4.price')
+        let price = await priceScrap.evaluate(el => el.innerText)
+
+        let nameScrap = await item.$('a.title')
+        let name = await nameScrap.evaluate(el => el.innerText)
+
+        let ratingScrap = await item.$('[data-rating]')
+        let rating = await ratingScrap.evaluate(el => el.dataset.rating)
+
+        let newItem = {
+            name: name,
+            price: price,
+            rating: rating
+        }
+
+        console.log(newItem)
+        //console.log(items)
+        items.list.push(newItem)
+    })
+    console.log(items.list)    
+    //.then(items => console.log(items))
+}
+
+// screenShotCantons();
+//showCantonsOnConsole();
+scrapECommerce()
