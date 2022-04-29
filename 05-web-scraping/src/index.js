9b7ba44db7d751a6fdc2f591f3ba77cce959fa8b
Timothée Dione <timothee.dione@gmail.com>
2022-04-29 09:33:14 +0200

    screen des cantons fonctionnel

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,3 +1,53 @@
-import * as puppeteer from "puppeteer";
+import jsdom from "jsdom";
+import fetch from "isomorphic-fetch"
+import puppeteer from "puppeteer"
 
-console.log("coucou")
+/*Cours_
+// Lancement browser
+const browser = await puppeteer.launch();
+//Ouvrir une nouvelle page
+  const page = await browser.newPage();
+  console.log(page)
+//Aller au lien example.com
+await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+ 
+//Sélecteurs
+const tableCantons = await page.$('table.wikitable'); // document.querySelector
+//await page.$$(sélecteur); // document.querySelectorAll
+// EXEMPLE : await page.$$(div);
+ 
+//Appliquer une fonction aux sélecteurs
+//await page.$$eval(sélecteur, pageFunction[...args]);
+// EXEMPLE : const divCount = await page.$$eval('div', (divs) => divs.length);
+ 
+//Cliquer sur un selecteur
+/*
+await page.click(sélecteur);
+
+
+//Faire un screenshot
+await tableCantons.screenshot({ path: '././img/photoDesCantons.png' });
+ 
+// Fermer les browser
+await browser.close();
+};
+*/
+
+//capture d'écran
+async function screenShotCantons () {
+    // Lancement browser
+      const browser = await puppeteer.launch();
+    //Ouvrir une nouvelle page
+      const page = await browser.newPage();
+      //console.log(page)
+    await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+     
+    //Sélecteurs
+    const tableCantons = await page.$('table.wikitable'); // document.querySelector
+    await tableCantons.screenshot({ path: '././img/photoDesCantons.png' }); 
+    // Fermer les browser
+    await browser.close();
+    };
+
+
+    screenShotCantons();
